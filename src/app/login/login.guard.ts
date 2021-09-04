113bb1688849457ad42744c3a0591015e7bb7f8e
Shivansh Kumar <shivansh.dbg@gmail.com>
2021-09-04 20:48:15 +0530

    make it better

---
+++ src/app/login/login.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const jwtToken = localStorage.getItem('jwtToken');
+    if (jwtToken) return true;
+    else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
